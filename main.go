a84db5a4dba602fd2dd2daa8a59b5b9157ec1e44
kuroGenki <kurochange0522@gmail.com>
2024-06-01 01:10:07 +0900

    環境構築

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	// "net/http"
+
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	  // ここからCorsの設定
+		r.Use(cors.New(cors.Config{
+			// アクセスを許可したいアクセス元
+			AllowOrigins: []string{
+					"http://localhost:3000",
+			},
+			// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+			AllowMethods: []string{
+					"POST",
+					"GET",
+					"OPTIONS",
+			},
+			// 許可したいHTTPリクエストヘッダ
+			AllowHeaders: []string{
+					"Access-Control-Allow-Credentials",
+					"Access-Control-Allow-Headers",
+					"Content-Type",
+					"Content-Length",
+					"Accept-Encoding",
+					"Authorization",
+			},
+			// cookieなどの情報を必要とするかどうか
+			AllowCredentials: true,
+			// preflightリクエストの結果をキャッシュする時間
+			MaxAge: 24 * time.Hour,
+		}))
+
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "test",
+		})
+	})
+	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
+}
---
+++ todo_frontend/src/views/TaskIndex.vue
@@ -0,0 +1,5 @@
+<template>
+  task
+</template>
+<script setup>
+</script>
